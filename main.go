956e953cc548e7965efcb27146caf7d897d1b001
unknown <57038093+laumonnier@users.noreply.github.com>
2024-12-05 17:48:49 +0100

    adding main.go, TO add the unity members, the artist image, the first output date, and the first concert!!! WIP

--- main.go
+++ main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"io/ioutil"
 	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 type artist struct {
-	Id int `json:"id"`
-	Image string `json:"image"`
-	Name string `json:"name"`
-	Members []string `json:"members"`
-	CreationDate int `json:"creationDate"`
-	FirstAlbum string `json:"firstAlbum"`
-	Locations string `json:"locations"`
-	ConcertDates string `json:"concertDates"`
-	Relations string `json:"relations"`
+	Id           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
 }
 
 func main() {
