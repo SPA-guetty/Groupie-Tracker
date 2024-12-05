4c53f71ef344f995423f4079f54f9fc5f55a5154
unknown <57038093+laumonnier@users.noreply.github.com>
2024-12-05 17:17:05 +0100

    changing in principal functionality, with deleting commentary code

--- main.go
+++ main.go
@@ -35,15 +35,3 @@ func main() {
 		fmt.Println(e.Name)
 	}
 }
-
-/*
-{"id":52,
-"image":"https://groupietrackers.herokuapp.com/api/images/thechainsmokers.jpeg",
-"name":"The Chainsmokers",
-"members":["Alexander Pall","Andrew Taggart","Matt McGuire","Tony Ann"],
-"creationDate":2008,
-"firstAlbum":"15-03-2014",
-"locations":"https://groupietrackers.herokuapp.com/api/locations/52",
-"concertDates":"https://groupietrackers.herokuapp.com/api/dates/52",
-"relations":"https://groupietrackers.herokuapp.com/api/relation/52"}
-*/
