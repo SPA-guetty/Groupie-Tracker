62c8a38a1468c269ff66f1f0ba5a14aaafdd1d3e
unknown <57038093+laumonnier@users.noreply.github.com>
2024-12-07 15:55:16 +0100

    adding tests and errors possibilities

--- autors/autors.go
+++ autors/autors.go
@@ -1,38 +1,67 @@
 package autors
 
 import (
-	"fmt"
-	"net/http"
-	"io/ioutil"
 	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
 )
 
-type artist struct {
-	Id int `json:"id"`
-	Image string `json:"image"`
-	Name string `json:"name"`
-	Members []string `json:"members"`
-	CreationDate int `json:"creationDate"`
-	FirstAlbum string `json:"firstAlbum"`
-	Locations string `json:"locations"`
-	ConcertDates string `json:"concertDates"`
-	Relations string `json:"relations"`
+type Artist struct {
+	Id           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"` // Utiliser int au lieu de string
+	FirstAlbum   string   `json:"firstAlbum"`
+	Locations    string   `json:"locations"`
+	ConcertDates string   `json:"concertDates"`
+	Relations    string   `json:"relations"`
 }
 
-func GetArtists() []artist {
+func GetArtists() ([]Artist, error) {
 	url := "https://groupietrackers.herokuapp.com/api/artists"
-	req, _ := http.NewRequest("GET", url, nil)
-	res, _ := http.DefaultClient.Do(req)
-	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-	var tab []artist
-	err := json.Unmarshal([]byte(body), &tab)
+	// Création de la requête HTTP
+	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		fmt.Println("Error:", err)
-		return nil
+		log.Printf("Erreur lors de la création de la requête: %v", err)
+		return nil, err
 	}
-	for _, e := range tab {
-		fmt.Println(e.Name)
+
+	// Envoi de la requête et réception de la réponse
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Printf("Erreur lors de la réception de la réponse: %v", err)
+		return nil, err
 	}
-	return tab
+	defer res.Body.Close()
+
+	// Vérifier si la réponse est un succès
+	if res.StatusCode != http.StatusOK {
+		log.Printf("Erreur: statut HTTP %d", res.StatusCode)
+		return nil, fmt.Errorf("Erreur HTTP %d: %s", res.StatusCode, res.Status)
+	}
+
+	// Lire le corps de la réponse
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Printf("Erreur lors de la lecture du corps de la réponse: %v", err)
+		return nil, err
+	}
+
+	// Décodage des données JSON dans une tranche d'artistes
+	var artists []Artist
+	err = json.Unmarshal(body, &artists)
+	if err != nil {
+		log.Printf("Erreur lors du décodage JSON: %v", err)
+		return nil, err
+	}
+
+	// Affichage des noms des artistes pour le débogage (facultatif)
+	for _, e := range artists {
+		fmt.Println("Artiste:", e.Name)
+	}
+
+	return artists, nil
 }
