ebfb79edff73e68f74f673667285935a0dbc887f
unknown <57038093+laumonnier@users.noreply.github.com>
2024-12-13 09:14:52 +0100

    Add filters.go function

---
+++ autors/filters.go
@@ -0,0 +1,35 @@
+package autors
+
+func Filter_By_Name(tab []Artist) []Artist {
+	var new_tab []Artist
+	for range tab {
+		firstval := tab[0]
+		firstint := 0
+		for i, e := range tab {
+			if e.Name < firstval.Name {
+				firstval = e
+				firstint = i
+			}
+		}
+		new_tab = append(new_tab, firstval)
+		tab = append(tab[:firstint], tab[(firstint+1):]...)
+	}
+	return new_tab
+}
+
+func Filter_By_Name_Reversed(tab []Artist) []Artist {
+	var new_tab []Artist
+	for range tab {
+		firstval := tab[0]
+		firstint := 0
+		for i, e := range tab {
+			if e.Name > firstval.Name {
+				firstval = e
+				firstint = i
+			}
+		}
+		new_tab = append(new_tab, firstval)
+		tab = append(tab[:firstint], tab[(firstint+1):]...)
+	}
+	return new_tab
+}
