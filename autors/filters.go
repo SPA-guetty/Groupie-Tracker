cdaaed214a19de8fddc0647f3db8d8832a45f4a2
unknown <57038093+laumonnier@users.noreply.github.com>
2024-12-16 17:04:00 +0100

    commit filters.go and main.go

--- autors/filters.go
+++ autors/filters.go
@@ -102,7 +102,7 @@ func Get_Between_Dates(targetmin string, targetmax string, tab []Artist) []Artis
 		data := concertdates.OpenDates(strconv.Itoa(artist.Id))
 		for _, date := range data.Dates {
 			date = Invert_Dates(date)
-			if (date >= targetmin && date <= targetmax) {
+			if date >= targetmin && date <= targetmax {
 				newtab = append(newtab, artist)
 				break
 			}
