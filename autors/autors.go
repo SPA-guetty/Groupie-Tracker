d9e5f5e0c4c646d537ae99d3420337c0122d6472
SPA-guetty <thomas@DESKTOP-P7QVO40>
2024-12-17 09:01:28 +0100

    Fix First Album

--- autors/autors.go
+++ autors/autors.go
@@ -14,6 +14,7 @@ type Artist struct {
 	Name         string   `json:"name"`
 	Members      []string `json:"members"`
 	CreationDate int      `json:"creationDate"`
+	FirstAlbum 	 string	  `json:"firstAlbum"`
 	Locations    string   `json:"locations"`
 	ConcertDates string   `json:"concertDates"`
 	Relations    string   `json:"relations"`
